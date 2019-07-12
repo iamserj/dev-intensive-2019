c17cd914e707cc141e219e314c75e866078939bb
iamserj <puzanius@gmail.com>
2019-07-12 22:41:57 +0300

    add toInitials

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -27,10 +27,23 @@ object Utils {
         //TODO("not implemented")
         return "asdf"
     }
+    /*
 
+     */
+    // 6. Implement method Utils.toInitials(firstName lastName) returning first letters in upper case.
+    // if one of them is null return the second one, if both null return null
     fun toInitials(firstName: String?, lastName: String?): String? {
-        //TODO("no implemented")
-        return "A.S.D.F."
+        // orEmpty() Returns the string if it is not null, or the empty string otherwise.
+        // getOrNull(index) Returns a character at the given index or null if the index is out of bounds of this char sequence.
+        val firstChar = firstName.orEmpty().trim().getOrNull(0)?.toUpperCase()
+        val lastChar = lastName.orEmpty().trim().getOrNull(0)?.toUpperCase()
+
+        // If the expression to the left of Elvis (?:) is not null, the Elvis operator returns it, otherwise it returns the expression to the right.
+        val firstI = firstChar?.toString() ?: ""
+        val lastI = lastChar?.toString() ?: ""
+
+        // ifEmpty { expression } returns string if it's not empty or the result of calling expression in curly braces.
+        return "$firstI$lastI".ifEmpty { null }
     }
 
 }
