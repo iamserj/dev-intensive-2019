bf71f72cc6698bb16d2800f83a8b4d609f62ad03
iamserj <puzanius@gmail.com>
2019-07-13 00:11:47 +0300

    add extensions String.truncate, String.stripHTML

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,23 @@
+package ru.skillbranch.devintensive.extensions
+
+/**
+ * @author iamserj
+ * 13.07.2019 0:03
+ */
+
+
+// Implement extension cutting string from 'length' to end and return cutted string with filling "..." (if string was cutted)
+// if last char of cutted string is space remove it
+fun String.truncate(length:Int = 16):String{
+    val trimmedMessage = this.trim()
+    return if (trimmedMessage.length <= length) trimmedMessage else trimmedMessage.substring(0, length).trim() + "..."
+}
+
+// Implement extension which is:
+// 1) removing html tags and html escape sequences ("& < > ' "")
+// 2) removing empty spaces between words if there are more than one space.
+fun String.stripHtml():String{
+    val htmlRegex = Regex("(<.*?>)|(&[^ а-я]{1,4}?;)")
+    val spaceRegex = Regex(" {2,}")
+    return this.replace(htmlRegex, "").replace(spaceRegex, " ")
+}
